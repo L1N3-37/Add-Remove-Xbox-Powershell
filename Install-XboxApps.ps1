7c02484e49345ebdad818da46cfbf86a7b775deb
Lxizx <luiscardona260@gmail.com>
2024-06-04 16:34:55 +0100

    This PowerShell script is designed to enable Xbox-related services on a Windows machine and open the Xbox app's Microsoft Store page, allowing users to easily install the app. Key actions performed by this script include: Clearing the terminal before execution to ensure a clean interface. Enabling Xbox-related services (Xbox Accessory Management Service, Xbox Live Networking Service, Gaming Services, and Gaming Services Network) by setting their startup type to Automatic and starting them if they're not already running. Informing users about the script's actions through clear and concise instructions. Opening the Microsoft Store page for the Xbox app, allowing users to quickly access and install the app. This script simplifies the process of preparing a Windows machine for the installation of the Xbox app by managing the necessary services and guiding users through the installation process.

---
+++ Install-XboxApps.ps1
@@ -0,0 +1,32 @@
+# Clear the terminal before execution
+Clear-Host
+
+# Instruction
+Write-Host "Enabling Xbox Services..."
+Start-Sleep -Seconds 1
+
+# Enable Xbox Services
+$services = @(
+"XboxGipSvc",				# Xbox Accessory Management Service
+"XboxNetApiSvc",			# Xbox Live Networking Service
+"GamingServices",			# Gaming Services
+"GamingServicesNet"			# Gaming Services Network
+)
+
+foreach ($service in $services) {
+    Get-Service -Name $service -ErrorAction SilentlyContinue | ForEach-Object {
+        Start-Service -Name $_.Name -ErrorAction SilentlyContinue
+        Set-Service -Name $_.Name -StartupType Automatic -ErrorAction SilentlyContinue
+    }
+}
+# Instruction
+Write-Host "Opening the Xbox App page in Microsoft Store..."
+Start-Sleep -Seconds 1
+
+# Instruction
+Write-Host "Click the Install button"
+Start-Sleep -Seconds 1
+
+# Open Microsoft Store
+$storeApp = "ms-windows-store://pdp/?productid=9MV0B5HZVK9Z"
+Start $storeApp
