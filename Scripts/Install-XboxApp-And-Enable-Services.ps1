f603fe34706faa73207ec8d30084a4c12cbf2fad
Lxizx <luiscardona260@gmail.com>
2024-06-04 20:52:40 +0100

    Script

--- Scripts/Install-XboxApp-And-Enable-Services.ps1
+++ Scripts/Install-XboxApp-And-Enable-Services.ps1
@@ -3,7 +3,6 @@ Clear-Host
 
 # Instruction
 Write-Host "`n--> Enabling Xbox Services..."
-Start-Sleep -Seconds 1
 
 # Enable Xbox Services
 $services = @(
@@ -23,13 +22,13 @@ foreach ($service in $services) {
 Write-Host "`n--> Opening the Xbox App page in Microsoft Store..."
 Start-Sleep -Seconds 1
 
-# Instruction
-Write-Host "`n--> Click the -[ Install ]- button when prompted"
-Start-Sleep -Seconds 2
-
 # Open Microsoft Store
 $storeApp = "ms-windows-store://pdp/?productid=9MV0B5HZVK9Z"
 Start $storeApp
+
+# Instruction
+Write-Host "`n--> Click the -[ Install ]- button when prompted"
+Start-Sleep -Seconds 2
 
 # Separation line
 Write-Host "`n---------------------------------------------------"
